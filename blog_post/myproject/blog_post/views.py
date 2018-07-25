7f4d45fbee8fd3ea28763e364a7a92e6dd3f19d6
nobin50 <nobin.mbstu@gmail.com>
2018-07-25 21:36:16 +0600

    blog_post project

---
+++ blog_post/myproject/blog_post/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    all_post = Post.objects.all()
+    return render(request, 'index.html', {'all_post_list': all_post})
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+
+    return render(request, 'post_list.html', {'post_list': post_list})
+# Create your views here.
+
+def single_post(request, post_id):
+   post = Post.objects.get(pk=post_id)
+   print(post)
+   return render(request, 'single_post.html', {'post': post})
---
+++ blog_post/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\md. nobin\desktop\blog_post\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
